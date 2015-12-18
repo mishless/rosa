b28b2bd0ef927640594b95cc5e594554b8bb5648
Mihica <mihovilvinkovic@gmail.com>
2015-12-18 16:45:32 +0100

    delay absolute tests

--- src/tests/clock_manager/clock_manager_tests.c
+++ src/tests/clock_manager/clock_manager_tests.c
@@ -86,7 +86,7 @@ void scm_delayRelative_01(void)
 
 void delay_and_check_for_wake_up(void)
 {
-	ROSA_DelayRelative(1);
+	ROSA_DelayRelative(0, 10);
 	send_success();
 }
 
@@ -214,26 +214,38 @@ void delay_long(void)
 void delay_medium(void)
 {
 	ROSA_DelayRelative(100);
+	
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_delay_long)
+		send_fail();
+	if (delayQueue.size != 1)
+		send_fail();
+	
+	send_success();
 }
 
 void delay_short(void)
 {
 	ROSA_DelayRelative(50);
+	
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_delay_medium)
+		send_fail();
+	if (delayQueue.size != 2)
+		send_fail();
+	
+	while (1);
 }
 
-void check_if_3_tasks_in_delay_queue(void)
+void check_state_of_tasks_in_delay_queue(void)
 {
 	PriorityQueue delayQueue = fetchDELAYqueue();
 	if (( *(delayQueue.data[0]) ).task != (Task*) task_delay_short)
 		send_fail();
-	if (( *(delayQueue.data[1]) ).task != (Task*) task_delay_long)
-		send_fail();
-	if (( *(delayQueue.data[2]) ).task != (Task*) task_delay_medium)
-		send_fail();
 	if (delayQueue.size != 3)
 		send_fail();
 	
-	send_success();
+	while (1);
 }
 
 void scm_delayRelative_08(void)
@@ -243,7 +255,7 @@ void scm_delayRelative_08(void)
 	ROSA_CreateTask(&delay_medium, TEST_NAME, STACK_SIZE, TEST_PRIORITY, &task_delay_medium);
 	ROSA_CreateTask(&delay_long, TEST_NAME, STACK_SIZE, LOW_PRIORITY, &task_delay_long);
 	ROSA_CreateTask(&delay_short, TEST_NAME, STACK_SIZE, LOW_PRIORITY - 1, &task_delay_short);
-	ROSA_CreateTask(&check_other_tasks_wakeup_time, TEST_NAME, STACK_SIZE, LOW_PRIORITY - 2, NULL);
+	ROSA_CreateTask(&check_state_of_tasks_in_delay_queue, TEST_NAME, STACK_SIZE, LOW_PRIORITY - 2, NULL);
 	ROSA_Start();
 }
 
@@ -267,42 +279,242 @@ void scm_delayRelative_09(void)
 	ROSA_Start();
 }
 
+//		ROSA_DelayAbsolute
+
+int is_absolute_delayed = 1;
+
+void delay_absolute_for_long_time_and_set_flag(void)
+{
+	ROSA_DelayAbsolute(0, -1);
+	is_absolute_delayed = 0;
+}
+
+void check_if_other_is_absolute_delayed(void)
+{
+	busy_wait(1000);
+	
+	if (is_absolute_delayed)
+		send_success();
+	else
+		send_fail();
+}
+
 void scm_delayAbsolute_01(void)
 {
+	send_id("SCM-ABSOLUTE-01");
 	
+	ROSA_CreateTask(&delay_absolute_for_long_time_and_set_flag, TEST_NAME, STACK_SIZE, TEST_PRIORITY, NULL);
+	ROSA_CreateTask(&check_if_other_is_absolute_delayed, TEST_NAME, STACK_SIZE, LOW_PRIORITY, NULL);
+	ROSA_Start();
+}
+
+void delay_absolute_and_check_for_wake_up(void)
+{
+	ROSA_DelayAbsolute(0, 10);
+	send_success();
 }
 
 void scm_delayAbsolute_02(void)
 {
+	send_id("SCM-ABSOLUTE-02");
 	
+	ROSA_CreateTask(&delay_absolute_and_check_for_wake_up, TEST_NAME, STACK_SIZE, TEST_PRIORITY, NULL);
+	ROSA_Start();
+}
+
+TaskHandle task_absolute_delay_myself;
+
+void absolute_delay_for_long_time(void)
+{
+	ROSA_DelayAbsolute(0, -1);
+}
+
+void check_if_absolute_delayed_task_is_in_delay_queue(void)
+{
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_absolute_delay_myself)
+		send_fail();
+	if (delayQueue.size != 1)
+		send_fail();
+	
+	send_success();
 }
 
 void scm_delayAbsolute_03(void)
 {
+	send_id("SCM-ABSOLUTE-03");
 	
+	ROSA_CreateTask(&absolute_delay_for_long_time, TEST_NAME, STACK_SIZE, TEST_PRIORITY, &task_absolute_delay_myself);
+	ROSA_CreateTask(&check_if_absolute_delayed_task_is_in_delay_queue, TEST_NAME, STACK_SIZE, LOW_PRIORITY, NULL);
+	ROSA_Start();
+}
+
+ROSA_TickCount ticks_before_absolute_delay;
+
+void write_current_ticks_and_delay_absolute(void)
+{
+	ticks_before_absolute_delay = ROSA_TimerTickCount();
+	ROSA_DelayAbsolute(0, 666666);
+}
+
+void check_absolute_delayed_tasks_wakeup_time(void)
+{
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task->wakeUpTime != ticks_before_absolute_delay + 666666)
+		send_fail();
+	
+	send_success();
 }
 
 void scm_delayAbsolute_04(void)
 {
+	send_id("SCM-ABSOLUTE-04");
 	
+	ROSA_CreateTask(&write_current_ticks_and_delay_absolute, TEST_NAME, STACK_SIZE, TEST_PRIORITY, NULL);
+	ROSA_CreateTask(&check_absolute_delayed_tasks_wakeup_time, TEST_NAME, STACK_SIZE, LOW_PRIORITY, NULL);
+	ROSA_Start();
+}
+
+void delay_absolute_and_check_for_how_long_have_i_been_delayed(void)
+{
+	int startTime = ROSA_TimerTickCount();
+	ROSA_DelayAbsolute(0, 1256);
+	
+	if (ROSA_TimerTickCount() == 1256)
+		send_success();
+	
+	send_fail();
 }
 
 void scm_delayAbsolute_05(void)
 {
+	send_id("SCM-ABSOLUTE-05");
 	
+	ROSA_CreateTask(&delay_absolute_and_check_for_how_long_have_i_been_delayed, TEST_NAME, STACK_SIZE, TEST_PRIORITY, NULL);
+	ROSA_Start();
+}
+
+void delay_absolute_and_check_delay_queue_after(void)
+{
+	ROSA_DelayAbsolute(0,100);
+	PriorityQueue DELAYqueue = fetchDELAYqueue();
+	if (DELAYqueue.size == 0)
+		send_success();
+	
+	send_fail();
 }
 
 void scm_delayAbsolute_06(void)
 {
+	send_id("SCM-ABSOLUTE-06");
 	
+	ROSA_CreateTask(&delay_absolute_and_check_delay_queue_after, TEST_NAME, STACK_SIZE, TEST_PRIORITY, NULL);
+	ROSA_Start();
+}
+
+TaskHandle task_absolute_delay_long, task_absolute_delay_medium, task_absolute_delay_short;
+
+void delay_long(void)
+{
+	ROSA_DelayRelative(150);
+}
+
+void delay_medium(void)
+{
+	ROSA_DelayRelative(100);
+	
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_delay_long)
+	send_fail();
+	if (delayQueue.size != 1)
+	send_fail();
+	
+	send_success();
+}
+
+void delay_short(void)
+{
+	ROSA_DelayRelative(50);
+	
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_delay_medium)
+	send_fail();
+	if (delayQueue.size != 2)
+	send_fail();
+	
+	while (1);
+}
+
+void delay_absolute_long(void)
+{
+	ROSA_DelayRelative(150);
+}
+
+void delay_absolute_medium(void)
+{
+	ROSA_DelayRelative(100);
+	
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_absolute_delay_long)
+		send_fail();
+	if (delayQueue.size != 1)
+		send_fail();
+	
+	send_success();
+}
+
+void delay_absolute_short(void)
+{
+	ROSA_DelayRelative(50);
+	
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_absolute_delay_medium)
+		send_fail();
+	if (delayQueue.size != 2)
+		send_fail();
+	
+	while(1);
+}
+
+void check_if_3_tasks_in_delay_queue(void)
+{
+	PriorityQueue delayQueue = fetchDELAYqueue();
+	if (( *(delayQueue.data[0]) ).task != (Task*) task_absolute_delay_short)
+		send_fail();
+	if (delayQueue.size != 3)
+		send_fail();
+	
+	while(1);
 }
 
 void scm_delayAbsolute_07(void)
 {
+	send_id("SCM-ABSOLUTE-07");
 	
+	ROSA_CreateTask(&delay_medium, TEST_NAME, STACK_SIZE, TEST_PRIORITY, &task_absolute_delay_medium);
+	ROSA_CreateTask(&delay_long, TEST_NAME, STACK_SIZE, LOW_PRIORITY, &task_absolute_delay_long);
+	ROSA_CreateTask(&delay_short, TEST_NAME, STACK_SIZE, LOW_PRIORITY - 1, &task_absolute_delay_short);
+	ROSA_CreateTask(&check_other_tasks_wakeup_time, TEST_NAME, STACK_SIZE, LOW_PRIORITY - 2, NULL);
+	ROSA_Start();	
+}
+
+void delay_absolute_to_past(void)
+{
+	busy_wait(100 * 1000);
+	int start_time = ROSA_TimerTickCount();
+	ROSA_DelayAbsolute(0,50);
+	
+	int difference = ROSA_TimerTickCount() - start_time;
+	if (difference == 1 || difference == 2)
+		send_success();
+		
+	send_fail();
 }
 
 void scm_delayAbsolute_08(void)
 {
+	send_id("SCM-ABSOLUTE-08");
 	
+	ROSA_CreateTask(&delay_absolute_to_past, TEST_NAME, STACK_SIZE, TEST_PRIORITY, NULL);
+	ROSA_Start();
 }
